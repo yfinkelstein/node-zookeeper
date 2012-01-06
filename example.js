d55616571add106be90746ff19815cba67fe1c90
Dave Dopson <ddopson@zynga.com>
2012-01-06 14:44:38 -0800

    update example.js

--- example.js
+++ example.js
@@ -1,20 +1,20 @@
-#!/usr/bin/env node
-
-var ZK = require ("./lib/zookeeper");
-var zk = new ZK();
-
-var connect  = (process.argv[2] || 'localhost:2181');
-
-zk.init ({connect:connect, timeout:200000, debug_level:ZK.ZOO_LOG_LEVEL_WARN, host_order_deterministic:false});
-zk.on ('connect', function (zkk) {
-    console.log ("zk session established, id=%s", zkk.client_id);
-    zkk.a_create ("/node.js1", "some value", ZK.ZOO_SEQUENCE | ZK.ZOO_EPHEMERAL, function (rc, error, path)  {
-        if (rc != 0) 
+var ZooKeeper = require ("./lib/zookeeper");
+var zk = new ZooKeeper({
+  connect: "localhost:2181"
+ ,timeout: 200000
+ ,debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARNING
+ ,host_order_deterministic: false
+});
+zk.connect(function (err) {
+    if(err) throw err;
+    console.log ("zk session established, id=%s", zk.client_id);
+    zk.a_create ("/node.js1", "some value", ZooKeeper.ZOO_SEQUENCE | ZooKeeper.ZOO_EPHEMERAL, function (rc, error, path)  {
+        if (rc != 0) {
             console.log ("zk node create result: %d, error: '%s', path=%s", rc, error, path);
-        else {
+        } else {
             console.log ("created zk node %s", path);
             process.nextTick(function () {
-                zkk.close ();
+                zk.close ();
             });
         }
     });
