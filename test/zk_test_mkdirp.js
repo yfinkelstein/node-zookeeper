7f2b867108248bd84d2894b41b959906cecc825c
David Trejo <david.daniel.trejo@gmail.com>
2012-04-13 21:12:00 -0400

    zk_test_mkdirp.js: delete dirs afterwards

--- test/zk_test_mkdirp.js
+++ test/zk_test_mkdirp.js
@@ -1,12 +1,13 @@
 var ZK = require ('../lib/zookeeper');
 var assert = require('assert');
 var log4js = require('log4js');
+var async = require('async');
 
-// 
+//
 // based on node-leader
 // https://github.com/mcavage/node-leader
 // callback(error, zookeeperConnection)
-// 
+//
 function startZK(options, callback) {
   if(typeof(options) !== 'object')
     throw new TypeError('options (Object) required');
@@ -19,7 +20,7 @@ function startZK(options, callback) {
   if(typeof(callback) !== 'function')
     throw new TypeError('callback (Function) required');
 
-  var log = options.log4js.getLogger('Election');
+  var log = options.log4js.getLogger('mkdirp-test');
   var zkLogLevel = ZK.ZOO_LOG_LEVEL_WARNING;
 
   if(log.isTraceEnabled())
@@ -65,9 +66,56 @@ if (require.main === module) {
       }
       // path already exists, do nothing :)
       assert.ok(stat);
-      console.log('TEST PASSED!', __filename);
-      process.exit(0);
-      // can't delete the path we just made, b/c haven't written rm -rf :)
+      return mkDirpAgain();
     });
   }
+  // tests that no errors are thrown if you mkdirp a bunch of dirs that exist
+  function mkDirpAgain(err, win) {
+    con.mkdirp(PATH, finish);
+  }
+  function finish(err) {
+    assert.ifError(err);
+    console.log('TEST PASSED!', __filename);
+
+    var dirs = [
+      '/mkdirp/test/path/of/death/n/destruction'
+    , '/mkdirp/test/path/of/death/n'
+    , '/mkdirp/test/path/of/death'
+    , '/mkdirp/test/path/of'
+    , '/mkdirp/test/path'
+    , '/mkdirp/test'
+    , '/mkdirp'
+    ];
+
+    // can't easily delete the path, b/c haven't written rm -rf :)
+    async.series([
+        function (cb) { con.a_delete_(dirs[0], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[1], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[2], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[3], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[4], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[5], 0, normalizeCb(cb)); }
+      , function (cb) { con.a_delete_(dirs[6], 0, normalizeCb(cb)); }
+    ], function(err, results) {
+      console.log('Deleted mkdirp`ed dirs: '+results.length+' of '+dirs.length);
+      process.exit(0);
+    });
+  }
+
+  // allows a callback that expects to be called like this:
+  //    cb()
+  // to instead be called like this:
+  //    cb(rc, error)
+  // and then have the original callback only be called with an Error, or
+  // nothing at all if there was no Error.
+  function normalizeCb(callback) {
+    return function(rc, error) {
+      if(!callback) return;
+      if(rc != 0) {
+        return callback(new Error('Zookeeper Error: code='+rc+'   '+error));
+      }
+      return callback(null, true);
+    };
+  }
+
 }
