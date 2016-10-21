78d24d72c8c4d8a272df3c6bba409a0993cf79de
Matt Lavin <matt.lavin@gmail.com>
2016-10-21 08:22:50 -0400

    Skip zookeeper source download if existing file matches SHA-1 hash

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,6 +4,7 @@ ROOT=`pwd`
 DEPS=$ROOT/deps
 BUILD=$ROOT/build/zk
 ZK_VERSION=3.4.8
+ZK_VERSION_SHA1=51b61611a329294f75aed82f3a4517a4b6ff116f
 ZK=zookeeper-$ZK_VERSION
 ZK_DEPS=$DEPS/zookeeper
 ZK_FILE=/tmp/$ZK.tar.gz
--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -4,14 +4,27 @@
 
 cd $DEPS
 
-echo "Downloading $ZK from $ZK_URL"
+# Fetch the SHA1 hash to see if a file download is really needed
+curl --fail --output $ZK_FILE.sha1 $ZK_URL.sha1 || wget $ZK_URL.sha1 -O $ZK_FILE.sha1
 
-curl -C - --fail --output $ZK_FILE $ZK_URL || wget --continue $ZK_URL -O $ZK_FILE
+pushd `dirname $ZK_FILE`
+shasum -c $ZK_FILE.sha1
+CHECKSUM_MATCHES=$?
+popd
 
-if [ $? != 0 ] ; then
-    echo "Unable to download zookeeper library"
-    exit 1
+if [ $CHECKSUM_MATCHES -eq 0 ]; then
+    echo "Skipping zookeeper source download because cached version is correct"
+else
+    echo "Downloading $ZK from $ZK_URL"
+
+    curl -C - --fail --output $ZK_FILE $ZK_URL || wget --continue $ZK_URL -O $ZK_FILE
+
+    if [ $? != 0 ] ; then
+        echo "Unable to download zookeeper library"
+        exit 1
+    fi
 fi
+
 
 rm -rf $ZK_DEPS
 
