caf2470c9d773fa4b0b2dd670d8b02fbb8373c39
davidvujic <david@vujic.se>
2018-09-25 22:35:53 +0200

    fix: handle SunOS scenario

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
-const env = require('./env.js');
+const os = require('os');
 const fs = require('fs');
 const shell = require('shelljs');
+const env = require('./env.js');
 
 function exec(action) {
     const res = shell.exec(action);
@@ -9,6 +10,24 @@ function exec(action) {
         shell.echo('Unable to build zookeeper library');
         shell.exit(1);
     }
+}
+
+function handleSunOS() {
+    const uname = shell.exec('uname -v');
+
+    if (uname.match('joyent_.*')) {
+        const res = shell.exec(`pkgin list | grep zookeeper-client-${env.ZK_VERSION}`);
+
+        if (res.code !== 0) {
+            shell.echo('You must install zookeeper before installing this module. Try:');
+            shell.echo(`pkgin install zookeeper-client-${env.ZK_VERSION}`);
+        }
+    }
+}
+
+if (os.platform() === 'SunOS') {
+    handleSunOS();
+    return;
 }
 
 const isAlreadyBuilt = fs.existsSync(`${env.BUILD}/lib/libzookeeper_st.la`);
