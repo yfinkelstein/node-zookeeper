4ab0c480453de48b196c08328fc12a416f2d3bf3
davidvujic <david@vujic.se>
2018-09-26 23:06:05 +0200

    prepublish to js file

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const http = require('http');
 const shell = require('shelljs');
+const decompress = require('decompress');
+const decompressTargz = require('decompress-targz');
 const env = require('./env.js');
 
 function download(url, destination) {
+    if (fs.existsSync(destination)) {
+        shell.echo(`${destination} already exists. Skipping download.`);
+
+        return Promise.resolve();
+    }
+
     const file = fs.createWriteStream(destination);
 
     return new Promise((resolve, reject) => {
@@ -31,12 +39,23 @@ function download(url, destination) {
 }
 
 shell.cd(env.DEPS);
+
 download(env.ZK_URL, env.ZK_FILE)
     .then(() => {
         shell.rm('-rf', env.ZK_DEPS);
 
+        decompress(env.ZK_FILE, `${env.ZK}/src/c`, {
+            plugins: [
+                decompressTargz()
+            ]
+        }).then(() => {
+            shell.echo('Files decompressed');
+            shell.mv(env.ZK, env.ZK_DEPS);
+            shell.echo('Files moved');
+        });
+
     })
     .catch((e) => {
-    shell.echo(`Unable to download zookeeper libray. Error: ${e.message}`);
-    shell.exit(1);
-});
+        shell.echo(`Unable to download zookeeper libray. Error: ${e.message}`);
+        shell.exit(1);
+    });
