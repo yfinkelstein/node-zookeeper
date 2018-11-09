434eb19d098f0db282863e88cab7a40e267bc7f9
davidvujic <david@vujic.se>
2018-11-09 18:38:32 +0100

    fix: windows - explicit processor architecture when building with cmake

--- scripts/build.js
+++ scripts/build.js
@@ -42,7 +42,7 @@ shell.cd(`${env.ZK_DEPS}/src/c`);
 
 
 if (os.platform().toLowerCase().includes('win32')) {
-    exec('cmake .');
+    exec(`cmake -DCMAKE_GENERATOR_PLATFORM=${os.arch()} .`);
     exec('cmake --build .');
 } else {
     exec('./configure --without-syncapi --enable-static --disable-shared --with-pic');
