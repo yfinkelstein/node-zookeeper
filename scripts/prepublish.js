3ee5126bdc0ae11a868f3a20f883936330eb8404
davidvujic <david@vujic.se>
2019-03-02 16:04:38 +0100

    refactor: windows apply patches, search-and-replace in files instead of entire file copy

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -77,11 +77,12 @@ function moveFolder() {
 
 function applyPatches() {
     if (env.isWindows) {
-        const source = `${env.rootFolder}/patches/windows`;
         const destination = `${env.sourceFolder}/src/c/src`;
-        shell.cp(`${source}/zookeeper.c`, `${destination}/zookeeper.c`);
-        shell.cp(`${source}/zk_log.c`, `${destination}/zk_log.c`);
-        shell.cp(`${source}/zk_adaptor.h`, `${destination}/zk_adaptor.h`);
+
+        shell.sed('-i', '#include "zookeeper_log.h"', '#include "zookeeper_log.h"\n#include "winport.h"\n', `${destination}/zk_log.c`);
+        shell.sed('-i', '#include "zookeeper.h"', '#include "winport.h"\n#include "zookeeper.h"\n', `${destination}/zk_adaptor.h`);
+        shell.sed('-i', '#include "zk_adaptor.h"', '#include "zk_adaptor.h"\n#include "winport.h"\n', `${destination}/zookeeper.c`);
+
         return;
     }
 
