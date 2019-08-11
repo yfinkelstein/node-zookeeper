adff114a795035f1e828c602e8022877fbfd9957
Jakub Bieńkowski <jbienkowski311@gmail.com>
2019-08-11 20:14:22 +0200

    Bring back tests

--- tests/zookeeper/exportedconstantstest.js
+++ tests/zookeeper/exportedconstantstest.js
@@ -1,8 +1,17 @@
 const test = require('tape');
 const NativeZk = require('./../../build/zookeeper.node').ZooKeeper;
 const constants = require('../../lib/constants.js');
+const ZooKeeper = require('../../lib/zookeeper.js');
 
 const keys = Object.keys(NativeZk);
+
+test('deprecated native static constants are exported', (t) => {
+    t.plan(keys.length);
+
+    keys.forEach((key) => {
+        t.equal(ZooKeeper[key], NativeZk[key]);
+    });
+});
 
 test('native static constants are exported', (t) => {
     t.plan(keys.length);
@@ -10,6 +19,20 @@ test('native static constants are exported', (t) => {
     keys.forEach((key) => {
         t.equal(constants[key], NativeZk[key]);
     });
+});
+
+test('deprecated legacy event constants are exported', (t) => {
+    t.plan(8);
+
+    t.equal(ZooKeeper.on_closed, 'close');
+    t.equal(ZooKeeper.on_connected, 'connect');
+    t.equal(ZooKeeper.on_connecting, 'connecting');
+    t.equal(ZooKeeper.on_event_created, 'created');
+
+    t.equal(ZooKeeper.on_event_deleted, 'deleted');
+    t.equal(ZooKeeper.on_event_changed, 'changed');
+    t.equal(ZooKeeper.on_event_child, 'child');
+    t.equal(ZooKeeper.on_event_notwatching, 'notwatching');
 });
 
 test('legacy event constants are exported', (t) => {
