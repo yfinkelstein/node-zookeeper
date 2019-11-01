3a684c82f1d7bd87d16d2fe43b41fdec7764a978
davidvujic <david@vujic.se>
2019-11-01 21:32:26 +0100

    WIP: zookeeper-3420

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -83,6 +83,7 @@ function applyPatches() {
         shell.sed('-i', '#include "zookeeper_log.h"', '#include "zookeeper_log.h"\n#include "winport.h"\n', `${destination}/zk_log.c`);
         shell.sed('-i', '#include "zookeeper.h"', '#include "winport.h"\n#include "zookeeper.h"\n', `${destination}/zk_adaptor.h`);
         shell.sed('-i', '#include "zk_adaptor.h"', '#include "zk_adaptor.h"\n#include "winport.h"\n', `${destination}/zookeeper.c`);
+        shell.sed('-i', 'if (buff == &zh->primer_buffer && rc == buff->len - 1) ++rc;', 'if (buff == &zh->primer_buffer && buff->curr_offset + rc == buff->len + sizeof(buff->len) - 1) ++rc;', `${destination}/zookeeper.c`);
 
         if (!env.isVerbose) {
             const cmakeFile = 'CMakeLists.txt';
