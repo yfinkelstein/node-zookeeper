1681ac1d0d1d7d24366980afe519eb7c97442fce
davidvujic <david@vujic.se>
2019-11-02 13:17:47 +0100

    WIP: windows support

--- scripts/env.js
+++ scripts/env.js
@@ -32,7 +32,7 @@ const suffix = '.tar.gz';
 let downloadedFileName;
 
 if (isWindows) {
-    zookeeperVersion = '3.4.13';
+    zookeeperVersion = '3.4.14';
     downloadedFolderName = `zookeeper-${zookeeperVersion}`;
     downloadedFileName = `${downloadedFolderName}${suffix}`;
     sha512sum = `b2e03d95f8cf18b97a46e2f53871cef5a5da9d5d80b97009375aed7fb35368c440ca944c7e8b64efabbc065f6fb98bb86239f7c1491f0490efc71876d5a7f424  ${downloadedFileName}`;
