2b67b2725cd4dfe2a3cfefbd021908114b968b95
davidvujic <david@vujic.se>
2020-12-20 19:30:38 +0100

    fix: use v3.5.8 of the ZooKeeper C Client with all platforms

---
+++ deps/README.md
@@ -0,0 +1,10 @@
+### Developer: updating the ZooKeeper Client C source code
+
+This guide is extracted from the [official docs](https://github.com/apache/zookeeper/blob/master/zookeeper-client/zookeeper-client-c/README)
+
+1 Download the source code
+2 Navigate to the root folder: run ant compile_jute 
+3 Navigate to the zookeeper-client-c folder: run autoreconf -if
+4 compress the entire zookeeper-client-c folder and name it `zookeeper-client-c.tar.gz`
+   * On Mac OS X: `COPYFILE_DISABLE=1 tar -czvf zookeeper-client-c.tar.gz zookeeper-client-c`
+5 replace the existing compressed file in the deps folder
--- scripts/env.js
+++ scripts/env.js
@@ -19,40 +19,21 @@ function checkIfAlreadyBuilt(env) {
 }
 
 const isWindows = process.platform.toLowerCase().includes('win32');
-const isSunOs = process.platform.toLowerCase().includes('sunos');
 const rootFolder = setRoot({ isWindows });
 
-let zookeeperVersion;
-let downloadedFolderName;
-let sha512sum;
-const suffix = '.tar.gz';
-let downloadedFileName;
-
-if (isWindows) {
-    zookeeperVersion = '3.4.14';
-    downloadedFolderName = `zookeeper-${zookeeperVersion}`;
-    downloadedFileName = `${downloadedFolderName}${suffix}`;
-    sha512sum = `b2e03d95f8cf18b97a46e2f53871cef5a5da9d5d80b97009375aed7fb35368c440ca944c7e8b64efabbc065f6fb98bb86239f7c1491f0490efc71876d5a7f424  ${downloadedFileName}`;
-} else {
-    zookeeperVersion = '3.5.6';
-    downloadedFolderName = `apache-zookeeper-${zookeeperVersion}`;
-    downloadedFileName = `${downloadedFolderName}${suffix}`;
-    sha512sum = `7f45817cbbc42aec5a7817fa2ae99656128e666dc58ace23d86bcfc5ca0dc49e418d1a7d1f082ad80ccb916f9f1b490167d16f836886af1a56fbcf720ad3b9d0  ${downloadedFileName}`;
-}
+const zookeeperVersion = '3.5.8';
+const downloadedFolderName = 'zookeeper-client-c';
+const downloadedFileName = 'zookeeper-client-c.tar.gz';
 
 const variables = {
     rootFolder,
     workFolder: `${rootFolder}/deps`,
     buildFolder: `${rootFolder}/build/zk`,
     zookeeperVersion,
-    sha512sum,
     downloadedFolderName,
-    sourceFolder: `${rootFolder}/deps/zookeeper`,
-    patchesFolder: `${rootFolder}/patches`,
+    sourceFolder: `${rootFolder}/deps/zookeeper-client-c`,
     downloadedFileName,
-    downloadUrl: `http://archive.apache.org/dist/zookeeper/zookeeper-${zookeeperVersion}/${downloadedFileName}`,
     isWindows,
-    isSunOs,
     isVerbose: !!process.env.ZK_INSTALL_VERBOSE,
 };
 
