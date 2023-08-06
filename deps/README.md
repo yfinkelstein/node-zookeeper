ab5fddc17d4c4d6ab5c9b21e46a653f2737f95fd
davidvujic <david@vujic.se>
2023-08-07 00:50:32 +0200

    wip: use zookeeper-c-client v3.8.2

--- deps/README.md
+++ deps/README.md
@@ -1,10 +1,11 @@
 ### Developer: updating the ZooKeeper Client C source code
 
 This guide is extracted from the [official docs](https://github.com/apache/zookeeper/blob/master/zookeeper-client/zookeeper-client-c/README)
+and the as of today unresolved issue about outdated docs: https://issues.apache.org/jira/browse/ZOOKEEPER-4621
 
-1 Download the source code
-2 Navigate to the `zookeeper-jute` folder and run `mvn compile`
-3 Navigate to the zookeeper-client-c folder: run autoreconf -if
-4 compress the entire zookeeper-client-c folder and name it `zookeeper-client-c.tar.gz`
+1. Download the source code
+2. Navigate to the `zookeeper-jute` folder and run `mvn compile`
+3. Navigate to the zookeeper-client-c folder: run autoreconf -if
+4. compress the entire zookeeper-client-c folder and name it `zookeeper-client-c.tar.gz`
    * On Mac OS X: `COPYFILE_DISABLE=1 tar -czvf zookeeper-client-c.tar.gz zookeeper-client-c`
-5 replace the existing compressed file in the deps folder
+5. replace the existing compressed file in the deps folder
--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -11,20 +11,6 @@ function clearPath() {
     shell.rm('-rf', env.sourceFolder);
 }
 
-function applyPatches() {
-    if (env.isWindows) {
-        const destination = `${env.sourceFolder}/src`;
-        shell.echo('Applying patches');
-        shell.sed('-i', '#include "zookeeper_log.h"', '#include "zookeeper_log.h"\n#include "winport.h"\n', `${destination}/zk_log.c`);
-        shell.sed('-i', '#include "zookeeper.h"', '#include "winport.h"\n#include "zookeeper.h"\n', `${destination}/zk_adaptor.h`);
-        shell.sed('-i', '#include "zk_adaptor.h"', '#include "zk_adaptor.h"\n#include "winport.h"\n', `${destination}/zookeeper.c`);
-        shell.sed('-i', /(FIPS_mode\(\) == 0)/, '0 == 0', `${destination}/zookeeper.c`);
-        shell.sed('-i', /(FIPS mode is OFF)/, 'Disabled the FIPS check', `${destination}/zookeeper.c`);
-    } else {
-        shell.exec(`patch -d ${env.rootFolder} -p0 --forward < ${env.workFolder}/no-fipsmode.patch`);
-    }
-}
-
 if (env.isAlreadyBuilt) {
     shell.echo('Zookeeper has already been built');
     shell.exit(0);
@@ -45,7 +31,6 @@ decompress(env.downloadedFileName, './', {
     ],
 }).then(() => {
     shell.echo('Decompressed file');
-    applyPatches();
 }).catch((e) => {
     shell.echo(`Error: ${e.message}`);
     shell.exit(1);
